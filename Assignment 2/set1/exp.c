54d4d93aca5c87e1195f559647b415b8286e77aa
mojimoon <cryovit@outlook.com>
2025-03-09 00:55:54 +0800

    code

---
+++ Assignment 2/set1/exp.c
@@ -0,0 +1,12 @@
+/* exp.c */
+#include <stdio.h>
+#include <stdlib.h>
+extern char **environ;
+void main() {
+    int i = 0;
+    while (environ[i] != NULL) {
+        printf("%s\n", environ[i]);
+    i++;
+    }
+    sleep(1);
+}
